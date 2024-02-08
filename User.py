a5a88673dd1489edebc80d3227aaccad858f94fc
ReginaFera13 <shawnmariesmith@icloud.com>
2024-02-07 21:05:02 -0500

    refactored for next challenege

--- User.py
+++ User.py
@@ -1,26 +1,96 @@
 class User:
-    def __init__(self, first_name, last_name, dob, email, license_num=None):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.dob = dob
-        self.email = email
-        self.license_num = license_num
+    all_users = []
+    total_users = 0
+    def __init__(self, username, first_name, last_name, dob, email, license_num=None): #first_name, last_name, dob, email, license_num=None
+        User.total_users += 1
+        self.set_username = username
+        User.all_users.append(self)
+        self.set_first_name = first_name
+        self.set_last_name = last_name
+        self.set_dob = dob
+        self.set_email = email
+        self.set_license_num = license_num
     
-    def full_name(self):
-        return f"{self.first_name} {self.last_name}"
-        
+    def __str__(self):
+        return f'\n{self.get_username}: {self.get_full_name} born on {self.get_dob} their email is {self.get_email} and their license number is {self.get_license_num}.'
+    
+    def __repr__(self):
+        return str(self)
+    
+    @property
+    def get_username(self):
+        return self._username
+    
+    @get_username.setter
+    def set_username(self, new_username):
+        if isinstance(new_username, str):
+            if new_username not in User.all_users:
+                self._username = new_username
+            else: 
+                print('Username taken. Try again')
+        else:
+            print('Username must be a string.')
+    
+    @property
+    def get_first_name(self):
+        return self._first_name
+    
+    @get_first_name.setter
+    def set_first_name(self, new_name):
+        if isinstance(new_name, str):
+            self._first_name = new_name.capitalize()
+        else:
+            print('Name must be a string.')
+            
+    @property
+    def get_last_name(self):
+        return self._last_name
+    
+    @get_last_name.setter
+    def set_last_name(self, new_name):
+        if isinstance(new_name, str):
+            self._last_name = new_name.capitalize()
+        else:
+            print('Name must be a string.')
+            
+    @property
+    def get_full_name(self):
+        self._full_name = f"{self.get_first_name} {self.get_last_name}"
+        return self._full_name
+    
+    @property
+    def get_dob(self):
+        return self._dob
+    
+    @get_dob.setter
+    def set_dob(self, birthday):
+        if isinstance(birthday, str):
+            self._dob = birthday.capitalize()
+        else:
+            print('Date of birth must be a string.')
+    
+    @property 
+    def get_email(self):
+        return self._email
+    
+    @get_email.setter
+    def set_email(self, new_email):
+        if isinstance(new_email, str):
+            self._email = new_email
+        else:
+            print('Email must be a string.')
+    
+    @property
+    def get_license_num(self):
+        return self._license_num
+    
+    @get_license_num.setter
+    def set_license_num(self, num):
+        if isinstance(num, int) or num is None:
+            self._license_num = num
+        else:
+            print('License number must be an integer or None.')
 
-
-mia = User('Mia', 'Johnson', '3 March 1987', 'miajohnson@gmail.com', '233761131483')
-alex = User('Alex', 'Rodriguez', '24 January 1976', 'alexrodriguez@yahoo.com', '630508017326')
-emily = User('Emily', 'Chen', '27 June 1999', 'emilychen@outlook.com')
-marcus = User('Marcus', 'Thompson', '11 August 1967', 'marcusthompson@icloud.com', '182710058534')
-
-print(mia.full_name())
-print(mia.email)
-print(alex.full_name())
-print(alex.email)
-print(emily.full_name())
-print(emily.email)
-print(marcus.full_name())
-print(marcus.email)
+user1 = User('ringbearer', 'frodo', 'baggins', 'september 22nd','frodobaggins@hotmail.com', 1234567890)
+user2 = User('gamgeegardens', 'samwise', 'gamgee', 'april 6th', 'samwise.gamgee@gmail.com') 
+print('all users:', User.all_users)
