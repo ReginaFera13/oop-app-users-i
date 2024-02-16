1a7640ce4f9969a85d268de6a166a1d6abe25b61
ReginaFera13 <shawnmariesmith@icloud.com>
2024-02-16 16:21:49 -0500

    Logon working; Profile menu done.

--- App.py
+++ App.py
@@ -11,7 +11,6 @@ class App:
     ]
     
     HOME = [
-        '---Welcome to ArdaConnect!---',
         '1. Profile',
         '2. Friends',
         '3. Posts',
@@ -20,14 +19,12 @@ class App:
     ]
     
     PROFILE =  [
-        '---Your Profile---',
         '1. Change username',
         '2. Change password',
         '3. Change first name',
         '4. Change last name',
         '5. Change email',
-        '6. Change license number',
-        '7. Menu'
+        '6. Menu'
     ]
     
     FRIENDS = [
@@ -53,29 +50,57 @@ class App:
         User.load_users_from_app(self)
     
     def load_data(self, file):
-        data_dict = {}
+        users = []
         with open(file, mode='r', newline='') as csvfile:
             user_data_reader = csv.DictReader(csvfile)
             for row in user_data_reader:
-                username = row['username']
-                    
-                user_data = {
-                    'username': username,
-                    'password': row['password'],
-                    'first_name': row['first_name'],
-                    'last_name': row['last_name'],
-                    'dob': row['dob'],
-                    'email': row['email'],
-                    'license_num': row['license_num']
-                }
-                data_dict[username] = user_data
-        return data_dict
+                user = User(
+                    username = row['username'],
+                    password = row['password'],
+                    first_name = row['first_name'],
+                    last_name = row['last_name'],
+                    dob = row['dob'],
+                    email = row['email'],
+                )
+                users.append(user)
+        return users
     
-    def home(self, user):
+    # def home(self, user):
+    #     menu_selction = 0
+    #     user = user
+    #     while menu_selction < 5:
+    #         for item in self.HOME:
+    #             print(item)
+            
+    #         if menu_selction == 0:
+    #             try:
+    #                 menu_selction = int(input('Enter menu number: '))
+    #             except ValueError:
+    #                 print('Invalid selection. Please try again.')
+    #                 menu_selction = 0
+    #         if menu_selction == 1:
+    
+    # def home(self, user):
+    #     menu_selction = 0
+    #     user = user
+    #     while menu_selction < 5:
+    #         for item in self.HOME:
+    #             print(item)
+            
+    #         if menu_selction == 0:
+    #             try:
+    #                 menu_selction = int(input('Enter menu number: '))
+    #             except ValueError:
+    #                 print('Invalid selection. Please try again.')
+    #                 menu_selction = 0
+    #         if menu_selction == 1:
+    
+    def profile(self, user):
         menu_selction = 0
         user = user
         while menu_selction < 5:
-            for item in App.HOME:
+            print(f'---Your Profile--- \nName: {user.get_full_name} \nUsername: {user.get_username} \nEmail: {user.get_email}')
+            for item in self.PROFILE:
                 print(item)
             
             if menu_selction == 0:
@@ -84,13 +109,57 @@ class App:
                 except ValueError:
                     print('Invalid selection. Please try again.')
                     menu_selction = 0
-        
+            if menu_selction == 1:
+                print(f'Current username: {user.get_username}')
+                user.set_username = input('Username: ')
+                print(f'New username: {user.get_username}')
+                menu_selction = 0
+            if menu_selction == 2:
+                print(f'Current password: {user.get_password}')
+                user.set_password = input('Password: ')
+                print(f'New password: {user.get_password}')
+                menu_selction = 0
+            if menu_selction == 3:
+                print(f'Current first name: {user.get_first_name}')
+                user.set_first_name = input('First name: ')
+                print(f'New first name: {user.get_first_name}')
+                menu_selction = 0
+            if menu_selction == 4:
+                print(f'Current last name: {user.get_last_name}')
+                user.set_last_name = input('Last name: ')
+                print(f'New last name: {user.get_last_name}')
+                menu_selction = 0
+            if menu_selction == 5:
+                print(f'Current email: {user.get_email}')
+                user.set_email = input('Email: ')
+                print(f'New email: {user.get_email}')
+                menu_selction = 0
+            if menu_selction == 6:
+                self.home(user)
+
     
+    def home(self, user):
+        menu_selction = 0
+        user = user
+        while menu_selction < 5:
+            print(f'---Welcome {user.get_first_name} to ArdaConnect!---',)
+            for item in self.HOME:
+                print(item)
+            
+            if menu_selction == 0:
+                try:
+                    menu_selction = int(input('Enter menu number: '))
+                except ValueError:
+                    print('Invalid selection. Please try again.')
+                    menu_selction = 0
+            if menu_selction == 1:
+                self.profile(user)
+                
     def run_app(self):
         menu_selction = 0
         user = None
         while menu_selction < 3:
-            for item in App.MENU:
+            for item in self.MENU:
                 print(item)
             
             if menu_selction == 0:
@@ -101,12 +170,12 @@ class App:
                     menu_selction = 0
             if menu_selction == 1:
                 user = User.get_user_by_username()
-                password = user.get_password
-                print(password)
-                menu_selction = 0
-                # password = User.password_login(user)
-                # if password == True:
-                #     self.HOME(user)
-                # else:
-                #     print('Login Failed: Your user ID or password is incorrect')
-                #     menu_selction == 1
+                if user:
+                    password = user.password_login()
+                    if password == True:
+                        print(user)
+                        self.home(user)
+                    else:
+                        print('Login Failed: Your user ID or password is incorrect')
+                        menu_selction == 1
+                    
--- User.py
+++ User.py
@@ -1,5 +1,5 @@
 class User:
-    all_users = {}
+    all_users = []
     
     @classmethod
     def load_users_from_app(cls, app):
@@ -8,93 +8,90 @@ class User:
     @classmethod
     def get_user_by_username(cls):
         username = input('Username: ')
-        user= cls.all_users.get(username)
-        if user is None:
-            print('User not found.')
-        return user
+        for user in cls.all_users:
+            if user.get_username == username:
+                return user
+        print('User not found.')
+        return None
     
     @classmethod
     def create_user(cls) -> dict:
         pass
 
-    def __init__(self, username, password, first_name, last_name, dob, email, license_num=None): #first_name, last_name, dob, email, license_num=None
+    def __init__(self, username, password, first_name, last_name, dob, email):
         User.total_users = len(User.all_users)
-        self.set_username = username
-        self.set_password = password
-        self.set_first_name = first_name
-        self.set_last_name = last_name
-        self.set_dob = dob
-        self.set_email = email
-        self.set_license_num = license_num
+        self._username = username
+        self._password = password
+        self._first_name = first_name
+        self._last_name = last_name
+        self._dob = dob
+        self._email = email
+
     
     def __str__(self):
-        return f'\n{self.get_username}: password: {self.get_password}, {self.get_full_name} born on {self.get_dob} their email is {self.get_email} and their license number is {self.get_license_num}.'
+        return f'\n{self.get_username}: password: {self.get_password}, {self.get_full_name} born on {self.get_dob} and their email is {self.get_email}.'
     
     def __repr__(self):
-        return f'\n{self.get_username}: password: {self.get_password}, {self.get_full_name} born on {self.get_dob} their email is {self.get_email} and their license number is {self.get_license_num}.'
+        return f'\n{self.get_username}: password: {self.get_password}, {self.get_full_name} born on {self.get_dob} and their email is {self.get_email}.'
     
     @property
     def get_username(self):
         return self._username
     
     @get_username.setter
-    def set_username(self):
-        new_username = input('Username: ')
+    def set_username(self, new_username):
         if isinstance(new_username, str):
-            if new_username not in User.all_users:
+            if new_username not in [user.get_username for user in User.all_users]:
                 self._username = new_username
             else: 
                 print('Username taken. Try again')
-                return self.set_username()
+                self.set_username = input('Username: ')
         else:
             print('Username must be a string.')
-            return self.set_username()
+            self.set_username = input('Username: ')
     
     @property
     def get_password(self):
         return self._password
     
     @get_password.setter
-    def set_password(self):
-        new_password = input('Password: ')
+    def set_password(self, new_password):
         if isinstance(new_password, str):
             self._password = new_password
         else:
             print('Password must be a string.')
-            return self.set_password()
+            self.set_password = input('Password: ')
     
-    # def password_login(self):
-    #     auth = False
-    #     password = input('Password: ')
-    #     if password == self.get_password:
-    #         auth = True
-    #     return auth
+    def password_login(self):
+        auth = False
+        password = input('Password: ')
+        if password == self.get_password:
+            auth = True
+        return auth
             
     @property
     def get_first_name(self):
         return self._first_name
     
     @get_first_name.setter
-    def set_first_name(self):
-        new_name = input('First name: ')
+    def set_first_name(self, new_name):
         if isinstance(new_name, str):
             self._first_name = new_name.capitalize()
         else:
             print('Name must be a string.')
-            return self.set_first_name()
+            self.set_first_name = input('First name: ')
             
     @property
     def get_last_name(self):
         return self._last_name
     
     @get_last_name.setter
-    def set_last_name(self):
-        new_name = input('Last name: ')
+    def set_last_name(self, new_name):
         if isinstance(new_name, str):
             self._last_name = new_name.capitalize()
         else:
             print('Name must be a string.')
-            return self.set_last_name()
+            self.set_last_name = input('Last name: ')
             
     @property
     def get_full_name(self):
@@ -106,36 +103,21 @@ class User:
         return self._dob
     
     @get_dob.setter
-    def set_dob(self):
-        new_dob = input('Date of birth: ')
+    def set_dob(self, new_dob):
         if isinstance(new_dob, str):
             self._dob = new_dob.capitalize()
         else:
             print('Date of birth must be a string.')
-            return self.set_dob()
+            self.set_dob = input('Date of birth: ')
     
     @property 
     def get_email(self):
         return self._email
     
     @get_email.setter
-    def set_email(self):
-        new_email = input('Email: ')
+    def set_email(self, new_email):
         if isinstance(new_email, str):
             self._email = new_email
         else:
             print('Email must be a string.')
-            return self.set_email()
-    
-    @property
-    def get_license_num(self):
-        return self._license_num
-    
-    @get_license_num.setter
-    def set_license_num(self):
-        num = int(input('License number: '))
-        if isinstance(num, int) or num is None:
-            self._license_num = num
-        else:
-            print('License number must be an integer or None.')
-            self.set_license_num()
+            self.set_email = input('Email: ')
